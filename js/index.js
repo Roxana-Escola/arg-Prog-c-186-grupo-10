946e72b3e5bea42ecc868eae1889e5adecb3c8f8
Roxana-Escola <roxanaescola@hotmail.com>
2023-04-09 20:38:15 -0300

    form desc + cod js p/mostrar
    termine formulario descuento
    creo funcion en js para aplicar dto
    oculto temporalmente cod js de contacto

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-/* 
-script para el form de contacto
-
-*/
+//script para el form de contacto (Ariel) INICIO//
 
 let d = document;
 
@@ -10,7 +7,7 @@ let btn = d.getElementById('btn_submit');
 let form = d.getElementById('contact_form');
 
 let datosRecibidos = d.getElementById('recepcion_datos')
-form.addEventListener('submit', enviarForm);
+form.addEventListener('submitDesc', enviarForm);
 
 function enviarForm(e) {
     e.preventDefault();
@@ -27,5 +24,31 @@ alert("Gracias por contactarnos, le responderemos a la brevedad")
 form.reset()
 }
 
+//script para el form de contacto (Ariel) FINAL//
 
 
+
+
+
+// --- Formulario Descuento (Roxana) -- INICIO
+
+//creo eventeo escuchar boton
+
+const formDesc = document.querySelector("#formDesc");
+
+
+
+formDesc.addEventListener("enviarFormDesc", validarformDesc)
+
+//creo funcion para generar y mostrar descuento
+
+function validarformDesc(e){
+    e.preventDefault();
+    const nombreDesc = document.querySelector("#nombreDesc").value
+    const dniDesc = document.querySelector("#dniDesc").value
+
+    const respuesta = document.getElementById("respuesta");
+    respuesta.textContent = `¡Felicitaciones ${nombreDesc} !  Ganaste un descuento del 5% en tu próximo servicio.  Sólo presentando tu dni nro: ${dniDesc} y nombrando el siguiente código: CUPON ${Math.random() * 100000000000000000}`
+}
+
+// --- Formulario Descuento (Roxana) -- FIN
