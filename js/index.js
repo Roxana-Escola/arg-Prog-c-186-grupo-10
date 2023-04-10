fac8ecf6e8e88d44e631b53f6381d8083d8db3b1
ariel <arirockseo@gmail.com>
2023-04-10 01:38:26 -0300

    correccion form ari

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 //script para el form de contacto (Ariel) INICIO//
 
+// NO CAMBIAR NADA DE ESTE FORM, POR FAVOR
+
 let d = document;
 
 let btn = d.getElementById('btn_submit');
@@ -7,7 +9,7 @@ let btn = d.getElementById('btn_submit');
 let form = d.getElementById('contact_form');
 
 let datosRecibidos = d.getElementById('recepcion_datos')
-form.addEventListener('submitDesc', enviarForm);
+form.addEventListener('submit', enviarForm);
 
 function enviarForm(e) {
     e.preventDefault();
@@ -19,7 +21,11 @@ let inputname = formInfo.get('name_input');
 let email = formInfo.get('email_input');
 let tArea = formInfo.get('area');
 
-datosRecibidos.textContent = `el cliente: ${inputname} email: ${email} deja su comenteario ${tArea}`;
+// ESTO SOLO SE MUESTRA POR CONSOLA POR EL MOMENTO
+
+console.log(`el cliente: ${inputname} 
+email: ${email} 
+deja su comenteario ${tArea}`);
 alert("Gracias por contactarnos, le responderemos a la brevedad")
 form.reset()
 }
