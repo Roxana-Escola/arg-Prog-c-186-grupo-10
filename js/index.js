d7f4eae392e6802aa61e67553f7e1843e19dccb9
Roxana-Escola <roxanaescola@hotmail.com>
2023-04-10 17:37:21 -0300

    agrego descuento.js
    agrego pagia descuento.js donde estan funciones formulario descuento y lo elimino de index.js y ahora funciona  bien

--- js/index.js
+++ js/index.js
@@ -43,28 +43,3 @@ form.reset()
 //script para el form de contacto (Ariel) FINAL//
 
 
-
-
-
-// --- Formulario Descuento (Roxana) -- INICIO
-
-//creo eventeo escuchar boton
-
-const formDesc = document.querySelector("#formDesc");
-
-
-
-formDesc.addEventListener("enviarFormDesc", validarformDesc)
-
-//creo funcion para generar y mostrar descuento
-
-function validarformDesc(e){
-    e.preventDefault();
-    const nombreDesc = document.querySelector("#nombreDesc").value
-    const dniDesc = document.querySelector("#dniDesc").value
-
-    const respuesta = document.getElementById("respuesta");
-    respuesta.textContent = `¡Felicitaciones ${nombreDesc} !  Ganaste un descuento del 5% en tu próximo servicio.  Sólo presentando tu dni nro: ${dniDesc} y nombrando el siguiente código: CUPON ${Math.random() * 100000000000000000}`
-}
-
-// --- Formulario Descuento (Roxana) -- FIN
