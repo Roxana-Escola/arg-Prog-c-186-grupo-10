78e4df9d83ed45b0cb3f241ead60e7a5db70cd35
ariel <arirockseo@gmail.com>
2023-04-10 01:56:22 -0300

    correccion form ari

--- js/index.js
+++ js/index.js
@@ -8,24 +8,34 @@ let btn = d.getElementById('btn_submit');
 
 let form = d.getElementById('contact_form');
 
-let datosRecibidos = d.getElementById('recepcion_datos')
+// let datosRecibidos = d.getElementById('recepcion_datos')
 form.addEventListener('submit', enviarForm);
+
+let name_input = d.getElementById('name_input');
+let email_input = d.getElementById('email_input')
+let tArea = d.getElementById('area')
 
 function enviarForm(e) {
     e.preventDefault();
 
-let formInfo = new FormData(form);
+// let formInfo = new FormData(form);
 
+// let inputname = formInfo.get('name_input');
+// let email = formInfo.get('email_input');
+// let tArea = formInfo.get('area');
 
-let inputname = formInfo.get('name_input');
-let email = formInfo.get('email_input');
-let tArea = formInfo.get('area');
+// Segunda forma de hacerlo
+
+let inputname = name_input.value;
+let email = email_input.value;
+let coment = tArea.value;
+
 
 // ESTO SOLO SE MUESTRA POR CONSOLA POR EL MOMENTO
 
 console.log(`el cliente: ${inputname} 
 email: ${email} 
-deja su comenteario ${tArea}`);
+deja su comenteario: ${coment}`);
 alert("Gracias por contactarnos, le responderemos a la brevedad")
 form.reset()
 }
